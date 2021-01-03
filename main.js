074272ac2ddc969abc13ba0a7aa4ff39dd4536d0
Sean Gray <sean.myles.gray@gmail.com>
2021-01-03 16:50:55 +0000

    added a validity check

--- main.js
+++ main.js
@@ -32,12 +32,30 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const prevBlock = this.chain[i - 1]
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        return false
+      }
+
+      if(currentBlock.previousHash !== prevBlock.hash) {
+        return false
+      }
+    }
+
+    return true
+  }
 }
 
 let grayCoin = new Blockchain()
 
 grayCoin.addBlock(new Block(1, "03/01/2021", { sender: "Sean gray", recipient: "Peter Gray", amount: 4 }))
-
 grayCoin.addBlock(new Block(2, "04/01/2021", { sender: "Kevin gray", recipient: "Myles Gray", amount: 10 }))
 
-console.log(JSON.stringify(grayCoin, null, 4))
+console.log('Is blockchain vaild? ' + grayCoin.isChainValid())
+
+// console.log(JSON.stringify(grayCoin, null, 4))
