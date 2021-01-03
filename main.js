cda453b8a882c8c8158bbb7113534b7cd87f10fc
Sean Gray <sean.myles.gray@gmail.com>
2021-01-03 14:50:47 +0000

    basic functionality. Console shows hashes but not data yet. Going to do research to rectify this

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import SHA256 from 'crypto-js/sha256'
+const SHA256 = require('crypto-js/sha256')
 
 
 class Block {
@@ -14,3 +14,31 @@ class Block {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
   }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()]
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2021', 'Genesis Block', '0')
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length -1]
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash
+    newBlock.hash = newBlock.calculateHash()
+    this.chain.push(newBlock)
+  }
+}
+
+let grayCoin = new Blockchain()
+
+grayCoin.addBlock(new Block(1, '03/01/2021', { amount: 4 }))
+
+grayCoin.addBlock(new Block(2, '04/01/2021', { amount: 10 }))
+
+console.log(JSON.stringify(grayCoin, null, 4))
