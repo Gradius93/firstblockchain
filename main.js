5c316ae705a25abc584063c9afd8fe910df533c8
Sean Gray <sean.myles.gray@gmail.com>
2021-01-03 16:18:04 +0000

    found the bug, was missing an r in the constructor declaration

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const SHA256 = require('crypto-js/sha256')
 
-
 class Block {
-  constuctor(index, timestamp, data, previousHash = '') {
+  constructor(index, timestamp, data, previousHash = '') {
     this.index = index
     this.timestamp = timestamp
     this.data = data
@@ -21,7 +20,7 @@ class Blockchain {
   }
 
   createGenesisBlock() {
-    return new Block(0, '01/01/2021', 'Genesis Block', '0')
+    return new Block(0, "01/01/2021", "Genesis Block", "0")
   }
 
   getLatestBlock() {
@@ -37,8 +36,8 @@ class Blockchain {
 
 let grayCoin = new Blockchain()
 
-grayCoin.addBlock(new Block(1, '03/01/2021', { amount: 4 }))
+grayCoin.addBlock(new Block(1, "03/01/2021", { sender: "Sean gray", recipient: "Peter Gray", amount: 4 }))
 
-grayCoin.addBlock(new Block(2, '04/01/2021', { amount: 10 }))
+grayCoin.addBlock(new Block(2, "04/01/2021", { sender: "Kevin gray", recipient: "Myles Gray", amount: 10 }))
 
 console.log(JSON.stringify(grayCoin, null, 4))
