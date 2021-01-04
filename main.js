5d0d0800976e690dfb3b14adf8aea0417ebdf19a
Sean Gray <sean.myles.gray@gmail.com>
2021-01-04 16:32:45 +0000

    added proof of work

--- main.js
+++ main.js
@@ -7,16 +7,26 @@ class Block {
     this.data = data
     this.previousHash = previousHash
     this.hash = this.calculateHash()
+    this.nonce = 0
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+  }
+
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++
+      this.hash = this.calculateHash()
+    }
+    console.log("Block mined: " + this.hash)
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()]
+    this.difficulty = 4
   }
 
   createGenesisBlock() {
@@ -29,7 +39,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash
-    newBlock.hash = newBlock.calculateHash()
+    newBlock.mineBlock(this.difficulty)
     this.chain.push(newBlock)
   }
 
@@ -53,9 +63,8 @@ class Blockchain {
 
 let grayCoin = new Blockchain()
 
+console.log("Mining block 1...")
 grayCoin.addBlock(new Block(1, "03/01/2021", { sender: "Sean gray", recipient: "Peter Gray", amount: 4 }))
+
+console.log("Mining block 2...")
 grayCoin.addBlock(new Block(2, "04/01/2021", { sender: "Kevin gray", recipient: "Myles Gray", amount: 10 }))
-
-console.log('Is blockchain vaild? ' + grayCoin.isChainValid())
-
-// console.log(JSON.stringify(grayCoin, null, 4))
